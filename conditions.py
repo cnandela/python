d76e98900138a75ef804c454d524550ea98ee26d
cnandela <48536886+cnandela@users.noreply.github.com>
2025-06-16 05:58:51 -0700

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -120,3 +120,33 @@ def cal_div():
 cal_div()
 
 
+
+#A company insures its drivers in the following cases:
+#If the driver is married.
+#If the driver is unmarried, male & above 30 years of age.
+#If the driver is unmarried, female & above 25 years of age.
+#In all other cases the driver is not insured. If the marital status, sex
+#and age of the driver are the inputs, write a program to determine
+#whether the driver is to be insured or not.
+
+def is_insured(martial_status, sex, age):
+  if martial_status == "married":
+      insured = True
+  elif martial_status == "unmarried" and sex == "male" and age > 30:
+      insured = True
+  elif martial_status == "unmarried" and sex == "female" and age > 25:
+      insured = True
+  else:
+      insured = False
+
+  return insured
+
+martial_status = input("martial status (married/unmarried): ").lower()
+sex = input("sex (male/female): ").lower()
+age = int(input("age: "))
+insured = is_insured(martial_status, sex, age)
+
+if insured:
+  print("driver is insured")
+else:
+  print("driver is not insured")
